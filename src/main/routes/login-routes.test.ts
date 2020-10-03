82dcc7654d1f2fb47e688a54626a7b4399bad7e6
Renan Rudney <renanrudney@gmail.com>
2020-10-03 16:50:24 -0300

    test: login route returns 401 with invalid credentials

--- src/main/routes/login-routes.test.ts
+++ src/main/routes/login-routes.test.ts
@@ -48,5 +48,15 @@ describe('Login Routes', () => {
         })
         .expect(200)
     })
+
+    test('Should return 401 on login', async () => {
+      await request(app)
+        .post('/api/login')
+        .send({
+          email: 'valid_email@mail.com',
+          password: 'valid_password'
+        })
+        .expect(401)
+    })
   })
 })
