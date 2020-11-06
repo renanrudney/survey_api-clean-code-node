bd437ec5ec4d9afb6ff9bd3ff3d960e18173060f
Renan Rudney <renanrudney@gmail.com>
2020-11-05 21:22:34 -0300

    feat: add LoadSurveyById usecase

--- src/domain/usecases/add-survey.ts
+++ src/domain/usecases/add-survey.ts
@@ -3,5 +3,5 @@ import { SurveyModel } from '@/domain/models/survey'
 export type AddSurveyModel = Omit<SurveyModel, 'id'>
 
 export interface AddSurvey {
-  add: (account: AddSurveyModel) => Promise<void>
+  add: (data: AddSurveyModel) => Promise<void>
 }
