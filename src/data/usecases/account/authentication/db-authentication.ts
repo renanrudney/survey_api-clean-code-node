56fc47cbaa7fd235615932aed5f4b1040fbd99ee
Renan Rudney <renanrudney@gmail.com>
2021-04-24 18:41:45 -0300

    feat: add name to authentication response

--- src/data/usecases/account/authentication/db-authentication.ts
+++ src/data/usecases/account/authentication/db-authentication.ts
@@ -1,5 +1,6 @@
 import {
   Authentication,
+  AuthenticationModel,
   AuthenticationParams,
   HashComparer,
   Encrypter,
@@ -15,14 +16,17 @@ export class DbAuthentication implements Authentication {
     private readonly updateAccessTokenRepository: UpdateAccessTokenRepository
   ) {}
 
-  async auth (authentication: AuthenticationParams): Promise<string> {
+  async auth (authentication: AuthenticationParams): Promise<AuthenticationModel> {
     const account = await this.loadAccountByEmailRepository.loadByEmail(authentication.email)
     if (account) {
       const isValid = await this.hashComparer.compare(authentication.password, account.password)
       if (isValid) {
         const accessToken = await this.encrypter.encrypt(account.id)
         await this.updateAccessTokenRepository.updateAccessToken(account.id, accessToken)
-        return accessToken
+        return {
+          accessToken,
+          name: account.name
+        }
       }
     }
     return null
--- src/domain/usecases/account/authentication.ts
+++ src/domain/usecases/account/authentication.ts
@@ -1,8 +1,10 @@
+import { AuthenticationModel } from '@/domain/models/authentication_model'
+
 export type AuthenticationParams = {
   email: string
   password: string
 }
 
 export interface Authentication {
-  auth: (authenticationParams: AuthenticationParams) => Promise<string>
+  auth: (authenticationParams: AuthenticationParams) => Promise<AuthenticationModel>
 }
