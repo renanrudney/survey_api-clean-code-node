60d1f2abc9858af4d82ceaa57288258015495836
Renan Rudney <renanrudney@gmail.com>
2021-07-25 17:21:54 -0300

    feat: add SurveyResult query and mutation

--- src/main/graphql/type-defs/index.ts
+++ src/main/graphql/type-defs/index.ts
@@ -1,5 +1,6 @@
 import base from './base'
 import login from './login'
 import surveys from './survey'
+import surveyResult from './survey-result'
 
-export default [base, login, surveys]
+export default [base, login, surveys, surveyResult]
